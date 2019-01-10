06bc2462a7a157d2c5d513573ff139ffb4d2473d
stitchthanadams <than@stitchdata.com>
2019-01-09 22:34:16 -0500

    update

--- Production Version.py/main.py
+++ Production Version.py/main.py
@@ -4,6 +4,7 @@ import csv
 from time import *
 from threading import Thread
 
+
 # this is for the timer
 state = False
 
@@ -12,31 +13,32 @@ start = True
 profile = False
 
 # >>>>>>>>>>>>>>>>>>>>>>> MAC ONLY
-blowerpwm = 0
-heaterpwm = 0
+# blowerpwm = 0
+# heaterpwm = 0
 
 # >>>>>>>>>>>>>>>>>>>>>>> RPI ONLY
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 
-# GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BOARD)
 
-# # assigning pin 12 - HEATER
-# GPIO.setup(12, GPIO.OUT)
-# GPIO.output(12, 0)
+# assigning pin 10 - HEATER
+GPIO.setup(10, GPIO.OUT)
+GPIO.output(10, 0)
 
-# # assigning pin 22 - BLOWER
-# GPIO.setup(22, GPIO.OUT)
-# GPIO.output(22, 0)
+# assigning pin 8 - BLOWER
+GPIO.setup(8, GPIO.OUT)
+GPIO.output(8, 0)
 
-# # starting PWM for heater
-# heaterpwm = GPIO.PWM(12, 60)
-# heaterpwm.start(0)
+# starting PWM for heater
+heaterpwm = GPIO.PWM(10, 60)
+heaterpwm.start(0)
 
-# # starting PWM for the blower
-# blowerpwm = GPIO.PWM(22, 58)
-# blowerpwm.start(0)
+# starting PWM for the blower
+blowerpwm = GPIO.PWM(8, 59)
+blowerpwm.start(0)
 
-
+air_now = 0
+heat_now = 0
 
 # GUI
 root = Tk()
@@ -46,9 +48,9 @@ root.wm_title('KALDI')
 root.geometry('500x350+30+30')
 
 bpwmdisp = StringVar()
-bpwmdisp.set(blowerpwm)
+bpwmdisp.set(air_now)
 hpwmdisp = StringVar()
-hpwmdisp.set(heaterpwm)
+hpwmdisp.set(heat_now)
 
 
 # try:
@@ -73,6 +75,8 @@ def setpwm(root, air, heat):
 	global start
 	global startint
 	global profile
+	global heat_now
+	global air_now
 	
 	if profile == False:
 		if start == True:
@@ -80,25 +84,29 @@ def setpwm(root, air, heat):
 			start = False
 			swstart()
 		else:
-			if blowerpwm == 0 and heaterpwm == 0:
+			if air_now == 0 and heat_now == 0:
 				logit(air, heat, time() - startint)
 			else:
-				logit(blowerpwm, heaterpwm, time() - startint)
+				logit(air_now, heat_now, time() - startint)
 			
 		startint = time()
+		
+		# blowerpwm = air
+		blowerpwm.ChangeDutyCycle(air)
+		air_now = air
 
-		blowerpwm = air
-		# blowerpwm.ChangeDutyCycle(air)
-
-		bpwmdisp.set(blowerpwm)
+		bpwmdisp.set(air_now)
 		jumptoair.delete(0, END)
 
-		heaterpwm = heat
-		# heaterpwm.ChangeDutyCycle(heat)
+		# heaterpwm = heat
+		heaterpwm.ChangeDutyCycle(heat)
+		heat_now = heat
 
-		hpwmdisp.set(heaterpwm)
+		hpwmdisp.set(heat_now)
 		jumptoheat.delete(0, END)
-	
+	else:
+                blowerpwm.ChangeDutyCycle(air)
+                heaterpwm.ChangeDutyCycle(heat)
 
 def control(root, bkill, hkill):
 	global blowerpwm
@@ -107,17 +115,17 @@ def control(root, bkill, hkill):
 
 	# assign the fields if they're not 0 or empty
 	if jumptoair.get() == '0' or not str.isdigit(jumptoair.get()):
-		newair = blowerpwm
+		newair = air_now
 	else:
 		newair = int(jumptoair.get())
 	
 	if jumptoheat.get() == '0' or not str.isdigit(jumptoheat.get()):
-		newheat = heaterpwm
+		newheat = heat_now
 	else:
 		newheat = int(jumptoheat.get())
 
 	# when both heater and blower are off
-	if blowerpwm == 0 and heaterpwm == 0:
+	if air_now == 0 and heat_now == 0:
 		if newair > 0 and newheat > 0:
 			setpwm(root, newair, newheat)
 		elif newair > 0:
@@ -128,9 +136,9 @@ def control(root, bkill, hkill):
 			print(msg)
 
 	# when the blower is on and the heater is off
-	elif blowerpwm > 0 and heaterpwm == 0:
+	elif air_now > 0 and heat_now == 0:
 		if bkill is True:
-			setpwm(root, 0, heaterpwm)
+			setpwm(root, 0, heat_now)
 		elif newair > 0 and newheat > 0:
 			setpwm(root, newair, newheat)
 		elif newair > 0 and newheat != 0:
@@ -141,9 +149,9 @@ def control(root, bkill, hkill):
 			print(msg)
 
 	# when both components are on
-	elif blowerpwm > 0 and heaterpwm > 0:
+	elif air_now > 0 and heat_now > 0:
 		if hkill is True and bkill is False:
-			setpwm(root, blowerpwm, 0)
+			setpwm(root, air_now, 0)
 		elif bkill is True and hkill is True:
 			setpwm(root, 0, 0)
 		elif newair > 0 and newheat > 0 and bkill is False:
@@ -160,6 +168,9 @@ def control(root, bkill, hkill):
 	
 
 def close_window(root):
+    control(root, True, True)
+    # RPi Only
+    GPIO.cleanup()
     exit()
 
 def killall(root):
@@ -200,9 +211,9 @@ def swreset():
 root.bind('<Control-Return>', go)
 
 def b_up(root):
-	global blowerpwm
-	if blowerpwm > 0:
-		new = blowerpwm + 1
+	global air_now
+	if air_now > 0:
+		new = air_now + 1
 		jumptoair.delete(0, END)
 		jumptoair.insert(0, int(new))
 		control(root, False, False)
@@ -212,9 +223,9 @@ def b_up(root):
 		jumptoheat.delete(0, END)
 
 def b_down(root):
-	global blowerpwm
-	if blowerpwm > 0:
-		new = blowerpwm - 1
+	global air_now
+	if air_now > 0:
+		new = air_now - 1
 		jumptoair.delete(0, END)
 		jumptoair.insert(0, int(new))
 		control(root, False, False)
@@ -223,14 +234,14 @@ def b_down(root):
 		jumptoair.delete(0, END)
 		jumptoheat.delete(0, END)
 
-root.bind('<Up>', b_up)
-root.bind('<Down>', b_down)
+root.bind('<w>', b_up)
+root.bind('<s>', b_down)
 
 
 def h_up(root):
-	global heaterpwm
-	if heaterpwm > 0:
-		new = heaterpwm + 1
+	global heat_now
+	if heat_now > 0:
+		new = heat_now + 1
 		jumptoheat.delete(0, END)
 		jumptoheat.insert(0, int(new))
 		control(root, False, False)
@@ -240,9 +251,9 @@ def h_up(root):
 		jumptoheat.delete(0, END)
 
 def h_down(root):
-	global heaterpwm
-	if heaterpwm > 0:
-		new = heaterpwm - 1
+	global heat_now
+	if heat_now > 0:
+		new = heat_now - 1
 		jumptoheat.delete(0, END)
 		jumptoheat.insert(0, int(new))
 		control(root, False, False)
@@ -251,19 +262,15 @@ def h_down(root):
 		jumptoair.delete(0, END)
 		jumptoheat.delete(0, END)
 
-root.bind('<w>', h_up)
-root.bind('<s>', h_down)
+root.bind('<Up>', h_up)
+root.bind('<Down>', h_down)
 
-
-
-nr = Label(root, text='KALDI', fg='#000000', font=('Helvetica', 75, 'bold'))
-# nr.place(width=490, height=95)
 
 # duty cycle displays
-d_blowlevel = Label(root, textvariable=bpwmdisp, bg='#75DAFF', fg='white', font=('Helvetica', 75, 'bold'))
+d_blowlevel = Label(root, textvariable=bpwmdisp, bg='#75DAFF', fg='white', font=('Helvetica', 60, 'bold'))
 d_blowlevel.place(x=500-250-125-75, y=10+30+50+10+30, height=70, width=150)
 
-d_heatlevel = Label(root, textvariable=hpwmdisp, bg='red', fg='white', font=('Helvetica', 75, 'bold'))
+d_heatlevel = Label(root, textvariable=hpwmdisp, bg='red', fg='white', font=('Helvetica', 60, 'bold'))
 d_heatlevel.place(x=500-125-75, y=10+30+50+10+30, height=70, width=150)
 
 
@@ -276,10 +283,10 @@ l_heatlevel.place(x=500-125-(78/2), y=10+50+10+10+30, height=20, width=78)
 
 
 # kill buttons
-kill_blower = Button(root, text='KILL', bg='#000000', fg='#00FFCC', command=lambda: control(root, True, False))
+kill_blower = Button(root, text='KILL', command=lambda: control(root, True, False))
 kill_blower.place(x=500-250-125-75, y=120+50-12+10+30, height=25, width=150)
 
-kill_heater = Button(root, text='KILL', bg='#000000', fg='#FF3333', command=lambda: control(root, False, True))
+kill_heater = Button(root, text='KILL', command=lambda: control(root, False, True))
 kill_heater.place(x=500-125-75, y=120+50-12+10+30, height=25, width=150)
 
 
@@ -311,10 +318,10 @@ def reset():
 	swreset()
 	print('intervals reset')
 
-reset_int = Button(root, text='Reset Intervals', bg='#000000', fg='#FF3333', command=reset)
+reset_int = Button(root, text='Reset Intervals', command=reset)
 reset_int.place(x=10, y=90+75+50+10+30-10+60+15, width=120, height=25)
 
-saveme = Button(root, text="Save as to csv..", bg="#FFFFFF", fg="#000000",command=saveit)
+saveme = Button(root, text="Save as to csv..", command=saveit)
 saveme.place(x=500-120-10-120-60+60, y=90+75+50+10+30-10+60+15, width=120, height=25)
 
 # this function opens a saved profile and loads it into blowerinterals/heaterintervals
@@ -328,70 +335,60 @@ def loadit():
     print("intervals loaded: ")
     print(intervals)
 
-loadme = Button(root, text="Load Profile...", bg="#FFFFFF", fg="#000000",command=loadit)
+loadme = Button(root, text="Load Profile...", command=loadit)
 loadme.place(x=500-120-10, y=90+75+50+10+30-10+60+15, width=120, height=25)
 
 def runit():
-		global intervals
-		global profile
+        global intervals
+        global profile
 
-		profile = True
+        profile = True
 
-		ti = len(intervals)
+        ti = len(intervals)
 
-		swstart()
-		for n, i in enumerate(intervals):
-			print(f'interval {n} of {ti}: {i}')
+        swstart()
 
-			jumptoair.delete(0, END)	
-			jumptoair.insert(0, int(round(i[0])))
-
-			jumptoheat.delete(0, END)
-			jumptoheat.insert(0, int(round(i[1])))
-
-			if int(round(i[0])) != 0 and int(round(i[1])) != 0:
-				control(root, False, False)
-			elif int(round(i[0])) != 0 and int(round(i[1])) == 0:
-				control(root, False, True)
-			elif int(round(i[0])) == 0 and int(round(i[1])) == 0:
-				control(root, True, True)
-			else:
-				continue
-
-			sleep(i[2])
-        
-		swpause()
-		profile = False
-		print('Done running the thing!')
-
-		jumptoair.delete(0, END)
-		jumptoheat.delete(0, END)
-		jumptoair.focus_set()
+        try:
+                for n, i in enumerate(intervals):
+                        print(f'interval {n} of {ti}: {i}')
+                        setpwm(root, int(round(i[0])), int(round(i[1])))
+                        bpwmdisp.set(int(round(i[0])))
+                        hpwmdisp.set(int(round(i[1])))
+                        sleep(i[2])
+        except KeyboardInterrupt:
+                pass
+        # as a safety measure, make sure both elements are at 0 after profile is finished
+        setpwm(root, 0, 0)
+        profile = False
+        swpause()
+        print('Done running the thing!')
+        hpwmdisp.set(0)
+        bpwmdisp.set(0)
+        jumptoair.delete(0, END)
+        jumptoheat.delete(0, END)
+        jumptoair.focus_set()
 
 def looprunner():
         runner_thread = Thread(target=runit)
         runner_thread.start()
 
-runme = Button(root, text="Run This Profile", bg="#FFFFFF", fg="#000000",command=looprunner)
+runme = Button(root, text="Run This Profile", command=looprunner)
 runme.place(x=10+120, y=90+75+50+10+30-10+60+15, width=120, height=25) 
 
-# directions = '''
-# set new levels:  control-return
-# up/down blower:  arrow up/arrow down
-# up/down heater:  w/s
-# stop roasting:   control-k
-# quit:            control-x
-# '''
-
-# ref = Label(root, text=directions, justify='left', font=('courier'))
-# ref.place(y=250+50+15, width=500)
+directions = '''
+set new levels:  control-return
+up/down blower:  arrow up/arrow down
+up/down heater:  w/s
+stop roasting:   control-k
+quit:            control-x
+'''
 
 #stop watch display label
 swDisp = Label(text="TIME", justify=LEFT, font=('Helvetica'))
 swDisp.place(x=500-250-(130/2), y=4+10, width=130, height=30)
  
 #STOP WATCH DISPLAY
-timeText = Label(root, text="00:00:00", font=("Helvetica", 40))
+timeText = Label(root, text="00:00:00", font=("Helvetica", 25))
 timeText.place(x=500-250-(160/2), y=10+5+5+5+10, width=160, height=40)
 
 global timer
@@ -402,13 +399,13 @@ pattern = '{0:02d}:{1:02d}:{2:02d}'
  
 #stopwatch buttons
  
-startButton = Button(root, text='Start', command=swstart, bg="#00CC66")
+startButton = Button(root, text='Start', command=swstart)
 startButton.place(x=500-250-(60/2)-65, y=30+35+10, height=25, width=60)
 
-resetButton = Button(root, text='Reset', command=swreset, bg="#FFFF00")
+resetButton = Button(root, text='Reset', command=swreset,)
 resetButton.place(x=500-250-(60/2), y=30+35+10, height=25, width=60)
 
-stopButton = Button(root, text='Stop', command=swpause, bg="#CC0000")
+stopButton = Button(root, text='Stop', command=swpause)
 stopButton.place(x=500-250-(60/2)+65, y=30+35+10, height=25, width=60)
  
 
