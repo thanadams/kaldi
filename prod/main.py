a6a7cab50c2f553fd5c3dc63ecfc79f92ddc6cda
thanadams <thanadams@gmail.com>
2025-10-13 17:43:45 -0400

    Add files via upload

--- prod/main.py
+++ prod/main.py
@@ -2,13 +2,31 @@ from tkinter import *
 from tkinter import filedialog
 import csv
 from time import *
+import datetime
 from threading import Thread
 import os
 
+#from yoctopuce.yocto_api import *
+#from yoctopuce.yocto_temperature import *
+
+from yoctopuce.yocto_api import *
+from yoctopuce.yocto_temperature import *
+from yoctopuce.yocto_relay import *
+
+
+#configure the temp sensor
+YAPI.RegisterHub("127.0.0.1")
+sensor = YTemperature.FirstTemperature()
+channel1 = YTemperature.FindTemperature('THRMCPL1-57DE2.temperature1')
+channel2 = YTemperature.FindTemperature('THRMCPL1-57DE2.temperature2')
+
+# this is the timer object - need to  check if it's not 0 before exiting the program
+temptimer = 0
 
 # this is for the timer
 state = False
 
+temps = []
 intervals = []
 start = True
 profile = False
@@ -27,7 +45,34 @@ bindings = '''
 	exit:  			[CONTROL] + [X]
 '''
 
-# >>>>>>>>>>>>>>>>>>>>>>> DEVELOPMENT RPI ONLY
+
+
+from threading import Timer
+
+class RepeatedTimer(object):
+    def __init__(self, interval, function):
+        self._timer     = None
+        self.interval   = interval
+        self.function   = function
+        self.is_running = False
+        self.start()
+
+    def _run(self):
+        self.is_running = False
+        self.start()
+        self.function()
+
+    def start(self):
+        if not self.is_running:
+            self._timer = Timer(self.interval, self._run)
+            self._timer.start()
+            self.is_running = True
+
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
+
+
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
@@ -47,7 +92,6 @@ heaterpwm.start(0)
 # starting PWM for the blower
 blowerpwm = GPIO.PWM(8, 59)
 blowerpwm.start(0)
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 
 # GUI
@@ -62,22 +106,56 @@ bpwmdisp.set(air_now)
 hpwmdisp = StringVar()
 hpwmdisp.set(heat_now)
 
+ch1 = round(channel1.get_currentValue())
+ch2 = round(channel2.get_currentValue())
+
+t1disp = StringVar()
+t1disp.set(ch1)
+t2disp = StringVar()
+t2disp.set(ch2)
+
 profiledisplayname = StringVar()
 profiledisplayname.set('none')
 
+def tempit():
+	global temps
+	ch1 = round(channel1.get_currentValue())
+	ch2 = round(channel2.get_currentValue())
+	t1disp.set(ch2)
+	t2disp.set(ch1) # this was on purpose
+	timetosend = time.ctime()
+	timetosend = datetime.datetime.strptime(time.ctime(), "%a %b %d %H:%M:%S %Y")
+	timetosend = timetosend.strftime("%H:%M:%S")
+	temps.append([ch2, ch1, timetosend])
+	print(f'CHANNEL1: {ch1} CHANNEL2: {ch2}')
+	time.sleep(1)
 
-def logit(air, heat, time):
+def logit(air, heat, time, time_of_log):
 	global intervals
-	intervals.append([air, heat, time])
+	intervals.append([air, heat, time, time_of_log])
 	print(f'new interval {len(intervals)}: {intervals[-1]}')
 
 def saveit():
-        global intervals
-        with filedialog.asksaveasfile(mode='w') as outfile:
-            writer = csv.writer(outfile)
-            writer.writerows(intervals)            
-        outfile.close()
-        print("Successfully printed everything to file!")    
+		global intervals
+		global temps
+
+		tstampsave = datetime.datetime.strptime(time.ctime(), "%a %b %d %H:%M:%S %Y")
+		tstampsave = tstampsave.strftime("%d%m%Y%H%M%S")
+
+		#save intervals
+		intervals_save = 'intervals - ' + tstampsave
+		with filedialog.asksaveasfile(mode='w', initialdir = "/share/profiles/", initialfile = intervals_save) as outfile:
+		    writer = csv.writer(outfile)
+		    writer.writerows(intervals)            
+		outfile.close()
+
+		#save temps
+		temps_save = 'temps - ' + tstampsave
+		with filedialog.asksaveasfile(mode='w', initialdir = "/share/profiles/", initialfile = temps_save) as outfile:
+		    writer = csv.writer(outfile)
+		    writer.writerows(temps)            
+		outfile.close()
+		print("Successfully printed everything to file!")    
 
 def setpwm(air, heat):
 	global blowerpwm
@@ -91,6 +169,7 @@ def setpwm(air, heat):
 	global heat_lower_bound
 	global blower_upper_bound 
 	global blower_lower_bound
+	global temptimer
 	
 	if air > blower_upper_bound or heat > heat_upper_bound: # or air < blower_lower_bound or heat < heat_lower_bound:
 		print('Out of bounds for the blower or heater!')
@@ -98,34 +177,40 @@ def setpwm(air, heat):
 	else:
 		if profile == False:
 			if start == True:
-				startint = time()
+				startint = time.time()
 				start = False
 				swstart()
+				temptimer = RepeatedTimer(1, tempit)
 			else:
 				if air_now == 0 and heat_now == 0:
-					logit(air, heat, time() - startint)
+
+					timetosend = time.ctime()
+					timetosend = datetime.datetime.strptime(time.ctime(), "%a %b %d %H:%M:%S %Y")
+					timetosend = timetosend.strftime("%H:%M:%S")
+
+					logit(air, heat, time.time() - startint, timetosend)
 				else:
-					logit(air_now, heat_now, time() - startint)
+
+					timetosend = time.ctime()
+					timetosend = datetime.datetime.strptime(time.ctime(), "%a %b %d %H:%M:%S %Y")
+					timetosend = timetosend.strftime("%H:%M:%S")
+
+					logit(air_now, heat_now, time.time() - startint, timetosend)
 				
-			startint = time()
+			startint = time.time()
 			
-			# >>>>>>>>>>>>>>>>>>>>>>> DEVELOPEMENT toggle the two lines below for development on a mac
-			# blowerpwm = air
 			blowerpwm.ChangeDutyCycle(air)
 
 			air_now = air
 			bpwmdisp.set(air_now)
 			jumptoair.delete(0, END)
 
-			# >>>>>>>>>>>>>>>>>>>>>>> DEVELOPEMENT toggle the two lines below for development on a mac
-			# heaterpwm = heat
 			heaterpwm.ChangeDutyCycle(heat)
 
 			heat_now = heat
 			hpwmdisp.set(heat_now)
 			jumptoheat.delete(0, END)
 
-		# >>>>>>>>>>>>>>>>>>>>>>> DEVELOPEMENT these two lines need to be commented to develop on a mac
 		else:
 			blowerpwm.ChangeDutyCycle(air)
 			heaterpwm.ChangeDutyCycle(heat)
@@ -221,10 +306,16 @@ def swreset():
 # this function opens a saved profile and loads it into blowerinterals/heaterintervals
 def loadit():
     global intervals
-    with filedialog.askopenfile(mode='r') as infile:
+    with filedialog.askopenfile(mode='r', initialdir = "/share/profiles/") as infile:
         reader = csv.reader(infile)
         rawimport = list(reader)
-        intervals = [list(map(float,rawimport)) for rawimport in rawimport]
+
+        print(rawimport)
+
+        for interval_set in rawimport:
+        	intervals.append([float(interval_set[0]),float(interval_set[1]),float(interval_set[2])])
+        print(intervals)
+        
         profiledisplayname.set(os.path.basename(infile.name))
         profile_name = os.path.basename(infile.name)
 
@@ -236,12 +327,14 @@ def loadit():
 def runit():
         global intervals
         global profile
+        global temptimer
 
         profile = True
 
         ti = len(intervals)
 
         swstart()
+        temptimer = RepeatedTimer(1, tempit)
 
         try:
                 for n, i in enumerate(intervals):
@@ -256,6 +349,7 @@ def runit():
         setpwm(0, 0)
         profile = False
         swpause()
+        temptimer.stop()
         print('Done running the thing!')
         hpwmdisp.set(0)
         bpwmdisp.set(0)
@@ -280,14 +374,17 @@ def reset():
 
 # These are the key bindings and their associated functions
 def close_window(gui):
-    control(True, True)
-    # >>>>>>>>>>>>>>>>>>>>>>> DEVELOPEMENT the 1 line blow needs to be commented out
-    GPIO.cleanup()
-    exit()
+	control(True, True)
+	if temptimer != 0:
+		temptimer.stop()
+	GPIO.cleanup()
+	exit()
 
 def killall(gui):
+	global temptimer
 	swpause()
 	control(True, True)
+	temptimer.stop()
 
 def go(gui):
 	control(False, False)
@@ -401,15 +498,39 @@ jumptoheat = Entry(background='red')
 jumptoheat.place(x=500-250-60, y=120+75+50+10+30-10, width=120, height=25)
 
 
-# label for the keymappings
-mappings = Label(text=bindings, anchor='center', font=('Helvetica', 9, 'italic'), justify=LEFT)
-mappings.place(x=15, y=335, height=75, width=420)
+# label for the keymappings - taking this out to display the temps I don't think I need this
+# mappings = Label(text=bindings, anchor='center', font=('Helvetica', 9, 'italic'), justify=LEFT)
+# mappings.place(x=15, y=335+25, height=75, width=420)
 
 
 # button to set both
 set_levels = Button(text='update\nlevels', command=lambda:control(False, False))
 set_levels.place(x=315, y=245, height=55, width=75)
 
+
+# temperature displays
+t1tempstage = Label(text="STAGE 1:", anchor='e', font=('Helvetica', 15))
+t1tempstage.place(x=500-250-60-15-100+10, y=245+65+5, height=60, width=100)
+
+t1temp = Label(textvariable=t1disp, bg='#f9d778', font=('Helvetica', 40, 'bold'))
+t1temp.place(x=500-250-60-15+10+7, y=245+65+10, height=50, width=115)
+
+t1tempdegf = Label(text="°F", anchor='w', font=('Helvetica', 15))
+t1tempdegf.place(x=500-250-60-15-100+10+220, y=245+57, height=60, width=100)
+
+
+t2tempstage = Label(text="STAGE 2:", anchor='e', font=('Helvetica', 15))
+t2tempstage.place(x=500-250-60-15-100+10, y=245+65+65-5, height=60, width=100)
+
+t2temp = Label(textvariable=t2disp, bg='#f9d778', font=('Helvetica', 40, 'bold'))
+t2temp.place(x=500-250-60-15+10+7, y=275+60+40, height=50, width=115)
+
+t2tempdegf = Label(text="°F", anchor='w', font=('Helvetica', 15))
+t2tempdegf.place(x=500-250-60-15-100+10+220, y=245+57+53, height=60, width=100)
+
+
+
+# buttons along bottom
 reset_int = Button(text='Reset Intervals', command=reset)
 reset_int.place(x=10, y=445, width=120, height=25)
 
@@ -427,9 +548,6 @@ closewindow.place(x=370, y=445+26, width=120, height=25)
 
 runme = Button(text="Run this profile", command=looprunner)
 runme.place(x=130, y=445, width=120, height=25) 
-
-
-
 
 ## START STOPWATCH ##
 global timer
